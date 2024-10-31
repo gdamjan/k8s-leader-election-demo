fdb403b514a284a1c71443a56a590339b730802b
Дамјан Георгиевски <gdamjan@gmail.com>
2024-10-31 05:35:55 +0100

    ruff format web.py

--- src/leader_election/web.py
+++ src/leader_election/web.py
@@ -3,14 +3,17 @@ import os
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return "<p>Hello, World!</p>"
+
 
 @app.route("/crash", methods=["POST"])
 def crash():
     # TODO: crash granian process
     os.kill(1, 9)
     return "crash"
+
 
 # TODO: run run_leader_election in a thread, use shared state
